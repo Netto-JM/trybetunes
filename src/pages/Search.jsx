2cfc2829974bffdb7ef2ae2864b69a3d0f1a7cf3
NettoJM <nettojm86@gmail.com>
2022-11-04 01:33:53 -0300

    implemented input and button on the "Search" component

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 
 function Search() {
+  const [artistName, setArtistName] = useState('');
+  const [isDisable, setIsDisable] = useState(true);
+
+  useEffect(() => {
+    setIsDisable(artistName.length < 2);
+  }, [artistName]);
+
   return (
     <div data-testid="page-search">
       <Header />
+      <input
+        type="text"
+        placeholder="Nome do Artista"
+        data-testid="search-artist-input"
+        value={ artistName }
+        onChange={ ({ target: { value } }) => { setArtistName(value); } }
+      />
+      <button
+        type="button"
+        data-testid="search-artist-button"
+        disabled={ isDisable }
+      >
+        Pesquisar
+      </button>
     </div>
   );
 }
