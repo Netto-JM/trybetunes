ffb7addc1646ef9d0d70a54cf0e8ef4b1d5aba94
NettoJM <nettojm86@gmail.com>
2022-11-04 17:35:21 -0300

    component created to list artists

---
+++ src/components/ArtistsList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ArtistsList(artistList) {
+  console.log('here:', artistList);
+  return (
+    <div>ArtistsList</div>
+  );
+}
+
+export default ArtistsList;
