e917690955fe675bd32ca7a8aee3033a0c9dd1b3
NettoJM <nettojm86@gmail.com>
2022-11-04 00:22:08 -0300

    added links to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -19,11 +20,18 @@ function Header() {
 
   return (
     <header data-testid="header-component">
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <h1 data-testid="header-user-name">{user}</h1>
-      )}
+      {isLoading ? <Loading /> : <h1 data-testid="header-user-name">{user}</h1>}
+      <nav>
+        <Link data-testid="link-to-search" to="/search">
+          Pesquisa
+        </Link>
+        <Link data-testid="link-to-favorites" to="/favorites">
+          Favoritos
+        </Link>
+        <Link data-testid="link-to-profile" to="/profile">
+          Perfil
+        </Link>
+      </nav>
     </header>
   );
 }
