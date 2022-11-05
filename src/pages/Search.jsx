a50db2e1ca977800155e830b779ee2374dc6e7f9
NettoJM <nettojm86@gmail.com>
2022-11-05 18:03:28 -0300

    change some variable names for readability

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,41 +4,41 @@ import Header from '../components/Header';
 import Loading from '../components/Loading';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
-const clickHandler = async (artistName, stateSetters) => {
+const clickHandler = async (searchInput, stateSetters) => {
   const [
     setIsLoading,
-    setArtistName,
+    setSearchInput,
     setSearchResults,
     setGotResults,
-    setArtistNameAfterClick,
+    setArtistName,
   ] = stateSetters;
-  setArtistName('');
+  setSearchInput('');
   setIsLoading(true);
-  const results = await searchAlbumsAPI(artistName);
+  const results = await searchAlbumsAPI(searchInput);
   setSearchResults(results);
-  setArtistNameAfterClick(artistName);
+  setArtistName(searchInput);
   setGotResults(true);
   setIsLoading(false);
 };
 
 function Search() {
+  const [searchInput, setSearchInput] = useState('');
   const [artistName, setArtistName] = useState('');
-  const [artistNameAfterClick, setArtistNameAfterClick] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [isDisable, setIsDisable] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [gotResults, setGotResults] = useState(false);
 
   useEffect(() => {
-    setIsDisable(artistName.length < 2);
-  }, [artistName]);
+    setIsDisable(searchInput.length < 2);
+  }, [searchInput]);
 
   const clkStateSetters = [
     setIsLoading,
-    setArtistName,
+    setSearchInput,
     setSearchResults,
     setGotResults,
-    setArtistNameAfterClick,
+    setArtistName,
   ];
 
   const showResults = gotResults && !isLoading;
@@ -54,21 +54,21 @@ function Search() {
             type="text"
             placeholder="Nome do Artista"
             data-testid="search-artist-input"
-            value={ artistName }
-            onChange={ ({ target: { value } }) => { setArtistName(value); } }
+            value={ searchInput }
+            onChange={ ({ target: { value } }) => { setSearchInput(value); } }
           />
           <button
             type="button"
             data-testid="search-artist-button"
             disabled={ isDisable }
-            onClick={ () => { clickHandler(artistName, clkStateSetters); } }
+            onClick={ () => { clickHandler(searchInput, clkStateSetters); } }
           >
             Pesquisar
           </button>
         </form>
       )}
       {showResults && (
-        <AlbumsList albumtList={ searchResults } artistName={ artistNameAfterClick } />
+        <AlbumsList albumtList={ searchResults } artistName={ artistName } />
       )}
     </div>
   );
