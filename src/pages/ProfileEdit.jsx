e3daf5af489279743fd5d47d9543e3233a724865
NettoJM <nettojm86@gmail.com>
2022-11-21 18:39:24 -0300

    using getUser API to fetch the user data and save in the component state

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 function ProfileEdit() {
+  const [userDescription, setUserDescription] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userImage, setUserImage] = useState('');
+  const [userName, setUserName] = useState('');
+
+  const fetchUser = async () => {
+    const resolve = await getUser();
+    const { description, email, image, name } = resolve;
+    setUserDescription(description);
+    setUserEmail(email);
+    setUserImage(image);
+    setUserName(name);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div data-testid="page-profile-edit">
       <Header />
