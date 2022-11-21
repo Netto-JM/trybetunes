4f6a639f29cb086f27ea326bcfa52039d905aa60
NettoJM <nettojm86@gmail.com>
2022-11-21 18:03:24 -0300

    getting and displaying user information, adding link to edit profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 function Profile() {
+  const [userDescription, setUserDescription] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userImage, setUserImage] = useState('');
+  const [userName, setUserName] = useState('');
+
+  const fetchUser = async () => {
+    const resolve = await getUser();
+    const { description, email, image, name } = resolve;
+    setUserDescription(description);
+    setUserEmail(email);
+    setUserImage(image);
+    setUserName(name);
+  };
+  fetchUser();
   return (
     <div data-testid="page-profile">
       <Header />
+      <img src={ userImage } alt={ userName } data-testid="profile-image" />
+      <h2>Nome</h2>
+      <p>{userName}</p>
+      <h2>E-mail</h2>
+      <p>{userEmail}</p>
+      <h2>Descrição</h2>
+      <p>{userDescription}</p>
+      <Link to="/profile/edit">
+        Editar perfil
+      </Link>
     </div>
   );
 }
