172834ef16b3fa21909d185556c9606cd32b2b15
NettoJM <nettojm86@gmail.com>
2022-11-21 19:27:06 -0300

    saving form inputs into state correctly, updating input values correctly, save button remains disabled until all inputs are filled in

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -7,6 +7,7 @@ function ProfileEdit() {
   const [userEmail, setUserEmail] = useState('');
   const [userImage, setUserImage] = useState('');
   const [userName, setUserName] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const fetchUser = async () => {
     const resolve = await getUser();
@@ -17,13 +18,75 @@ function ProfileEdit() {
     setUserName(name);
   };
 
+  const changeHandler = ({ target: { name, value } }) => {
+    switch (name) {
+    case 'image':
+      setUserImage(value);
+      break;
+    case 'name':
+      setUserName(value);
+      break;
+    case 'email':
+      setUserEmail(value);
+      break;
+    case 'description':
+      setUserDescription(value);
+      break;
+    default:
+      break;
+    }
+  };
+
+  const clickHandler = () => {
+    // some code
+  };
+
   useEffect(() => {
     fetchUser();
   }, []);
 
+  useEffect(() => {
+    setIsDisabled(!(userDescription && userEmail && userImage && userName));
+  }, [userDescription, userEmail, userImage, userName]);
+
   return (
     <div data-testid="page-profile-edit">
       <Header />
+      <form>
+        <img src={ userImage } alt={ userName } data-testid="profile-image" />
+        <input
+          data-testid="edit-input-image"
+          name="image"
+          value={ userImage }
+          onChange={ changeHandler }
+        />
+        <input
+          data-testid="edit-input-name"
+          name="name"
+          value={ userName }
+          onChange={ changeHandler }
+        />
+        <input
+          data-testid="edit-input-email"
+          name="email"
+          value={ userEmail }
+          onChange={ changeHandler }
+        />
+        <input
+          data-testid="edit-input-description"
+          name="description"
+          value={ userDescription }
+          onChange={ changeHandler }
+        />
+        <button
+          data-testid="edit-button-save"
+          type="button"
+          disabled={ isDisabled }
+          onClick={ clickHandler }
+        >
+          Salvar
+        </button>
+      </form>
     </div>
   );
 }
